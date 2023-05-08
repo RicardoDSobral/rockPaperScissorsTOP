5cc8a16068f50d5f2762d0f412c53d6366dc34c0
RicardoDSobral <ricardoduransobral@gmail.com>
2023-05-08 13:30:49 -0300

    Setting upe the folders + Pseudorandom algorithm
    - Uses a pseudorandom number generated from Math method
    - Assign each pseudorandom number to a choice by using a modulos div and a switch statement

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+// Get Computer choice by a algorithm thar returns a random number
+function getComputerChoice(){
+    // Using a 
+    let randomNumber = Math.floor(Math.random() * 300);
+    let computerChoice = randomNumber % 3;
+    switch (computerChoice){
+        case 0:{
+            return "rock";
+        }
+        case 1:{
+            return "paper";
+        }
+        case 2:{
+            return "scissor";
+        }
+    }
+}
+
+
+
+
